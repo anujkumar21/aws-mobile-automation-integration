f2dca4b117e716135e03018e01f33351739da4fd
Anuj Kumar <cdac.anuj@gmail.com>
2018-05-03 23:39:56 +0530

    aws & mobile automation integration
    Signed-off-by: Anuj Kumar <cdac.anuj@gmail.com>

--- src/test/java/auto/utils/BaseTest.java
+++ src/test/java/auto/utils/BaseTest.java
@@ -2,6 +2,9 @@ package auto.utils;
 
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.remote.AndroidMobileCapabilityType;
+import io.appium.java_client.remote.AutomationName;
+import io.appium.java_client.remote.MobileCapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
@@ -24,7 +27,7 @@ public class BaseTest {
     public void setUp() throws Exception {
         try {
             caps = new DesiredCapabilities();
-            /*caps.setCapability(MobileCapabilityType.PLATFORM_NAME, "android");
+            caps.setCapability(MobileCapabilityType.PLATFORM_NAME, "android");
             caps.setCapability(MobileCapabilityType.DEVICE_NAME, "knapster");
             caps.setCapability(MobileCapabilityType.PLATFORM_VERSION, "6.0");
             caps.setCapability(AndroidMobileCapabilityType.APP_PACKAGE, "org.wordpress.android");
@@ -32,7 +35,7 @@ public class BaseTest {
                     "org.wordpress.android.ui.WPLaunchActivity");
             caps.setCapability(MobileCapabilityType.APP,
                     System.getProperty("user.dir") + "/wordpress.apk");
-            caps.setCapability(MobileCapabilityType.AUTOMATION_NAME, AutomationName.ANDROID_UIAUTOMATOR2);*/
+            caps.setCapability(MobileCapabilityType.AUTOMATION_NAME, AutomationName.ANDROID_UIAUTOMATOR2);
             driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), caps);
         } catch (Exception ex) {
             System.out.println("Exception.");
